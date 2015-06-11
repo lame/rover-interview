322695c6da154083e315484a025ae0c8f928860e
Ryan Kuhl <rkk09c@gmail.com>
2015-06-11 09:55:12 -0400

    debugged controller and services, imports have been corrected, ngApp is now instantiating

---
+++ client/app/scripts/app.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+
+    angular.module('RoverApp', [
+        'ngRoute',
+        'smart-table',
+        'ui.router',
+    ])
+})();
--- client/app/scripts/controllers/search_sitters_controller.js
+++ client/app/scripts/controllers/search_sitters_controller.js
@@ -1,16 +1,9 @@
-angular.module('RoverApp').controller('SearchSittersController', ['$scope', '$http', 'SearchSittersService', function($scope, SearchSittersService) {
+angular.module('RoverApp').controller('SearchSittersController', ['$scope', '$http', 'searchSittersService', function($scope, searchSittersService) {
     'use strict';
     var ctrl = this;
 
     ctrl.getSitters = function() {
-
+        searchSittersService.getSitters()
+            .success(function(sitters) {$scope.sitters = sitters})
     }
-})
-
-// app.controller('basicsCtrl', ['$scope', function (scope) {
-//     scope.rowCollection = [
-//         {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-//         {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-//         {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
-//     ];
-// }]);
+}]);
