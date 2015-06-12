33694a16ef0e34f75bc62d40c2b56b4ae5817474
Ryan Kuhl <rkk09c@gmail.com>
2015-06-11 21:06:47 -0400

    smart table is now rendering, working on sorting and need to thread python processes

--- client/app/scripts/controllers/search_sitters_controller.js
+++ client/app/scripts/controllers/search_sitters_controller.js
@@ -1,17 +1,17 @@
-angular.module('RoverApp').controller('SearchSittersController', ['$scope', '$http', function(SearchSittersController, $scope, $http){
+angular.module('RoverApp').controller('SearchSittersController', ['$scope', '$http', '$filter', 'searchSittersService', function($scope, $http, $filter, searchSittersService){
     var ctrl = this;
-    ctrl.sitters = {};
 
     ctrl.getSitters = function() {
         searchSittersService.getSitters()
             .success(function(data, status, headers, config) {
                 console.log('success', status)
-                console.log('data', data)
-                ctrl.sitters = data
+                console.log('sitters data', data)
+                $scope.rowCollection = data
             })
             .error(function(data, status, headers, config) {
                 console.log('error', status)
             });
-        }
-}]);
+        };
 
+    ctrl.getSitters();
+}]);
