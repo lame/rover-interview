60c8786d77134aa8514f59cfb437e3ea2b71b1fc
Ryan Kuhl <rkk09c@gmail.com>
2015-06-13 23:38:54 -0400

    updated lookup tables to support by name rathen than by id, updated ranking algorithm for correctness

--- rover/models/owner_name_by_id.py
+++
@@ -1,8 +0,0 @@
-from cqlengine import columns, Model
-
-
-class OwnerNameByID(Model):
-    __table_name__ = 'owner_name_by_id'
-
-    owner_id = columns.Integer(primary_key=True)
-    owner_name = columns.Text()
